03edcf7dd58b605940df027a71ee73b2b7cc3651
Xudong Zhang <zxdvd_zhang@163.com>
2019-09-23 19:52:44 +0800

    add a script to generate index

---
+++ README.md
@@ -0,0 +1,11 @@
+
+### Notes
+
+* [ssh config的使用技巧](./content/shell/ssh+config的使用技巧.md)
+* [生产力: ssh跳板机配置](./content/shell/生产力:+ssh跳板机配置.md)
+* [basic: 为啥不建议用ps ef查看进程](./content/shell/basic:+为啥不建议用ps+ef查看进程.md)
+* [package-manage-cheatsheet](./content/shell/package-manage-cheatsheet.md)
+* [pg-basic: null的一些注意事项](./content/postgres/pg-basic:+null的一些注意事项.md)
+* [mysql-note](./content/postgres/mysql-note.md)
+* [pg-basic: 表名字段名是大小写问题](./content/postgres/pg-basic:+表名字段名是大小写问题.md)
+* [sql-cheatsheet](./content/postgres/sql-cheatsheet.md)
---
+++ gen_index.py
@@ -0,0 +1,34 @@
+
+import os
+
+def walk_dir(folder):
+    posts = []
+    with os.scandir(folder) as it:
+        for entry in it:
+            if entry.is_dir():
+                yield from walk_dir(entry.path)
+            if entry.is_file():
+                yield entry
+
+def gen_index(folder, template, target, placeholder):
+    links = []
+    for entry in walk_dir(folder):
+        if not entry.name.endswith('.md'):
+            continue
+        name = entry.name[:-3]
+        link = './' + entry.path
+        link = link.replace(' ', '+')
+        post = '* [%s](%s)' % (name, link)
+        links.append(post)
+    index = '\n'.join(links)
+    with open(template, 'r') as f1, open(target, 'w') as f2:
+        content = f1.read()
+        content = content.replace(placeholder, index)
+        f2.write(content)
+
+if __name__ == '__main__':
+    folder = os.getenv('folder', '.')
+    template = os.getenv('template', 'README.md.tmpl')
+    target = os.getenv('target', 'README.md')
+    placeholder = os.getenv('placeholder', '{{__index__}}')
+    gen_index(folder, template, target, placeholder)
