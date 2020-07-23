3d165860d59e0d26fec5051da75ea662f786b187
Xudong Zhang <zxdvd_zhang@163.com>
2020-07-23 16:08:26 +0800

    index: use git ls-files so that local drafts are not included

--- gen_index.py
+++ gen_index.py
@@ -1,28 +1,30 @@
 
 import os
+import subprocess
 
 from urllib.parse import quote
 
-def walk_dir(folder):
-    posts = []
-    with os.scandir(folder) as it:
-        for entry in sorted(it, key=lambda e: e.path):
-            if entry.is_dir():
-                yield from walk_dir(entry.path)
-            if entry.is_file():
-                yield entry
+def get_all_git_files():
+    out = subprocess.run(['git', 'ls-files', '-z'], capture_output=True, encoding='utf8')
+    return out.stdout.split('\0')
 
-def gen_index(folder, template, target, placeholder):
-    links = []
-    for entry in walk_dir(folder):
-        if not entry.name.endswith('.md'):
-            continue
-        name = entry.name[:-3]
-        link = './' + entry.path
-        pdir = os.path.basename(os.path.dirname(link))
-        name = '%s / %s' % (pdir, name)
-        post = '* [%s](%s)' % (name, quote(link))
-        links.append(post)
+def get_articles(folder='content', ext='.md'):
+    articles = get_all_git_files()
+    articles = [a for a in articles if a.startswith(folder) and a.endswith(ext)]
+    articles.sort()
+    return articles
+
+def gen_link(filename):
+    link = quote('./' + filename)
+    category = os.path.basename(os.path.dirname(filename))
+    basename = os.path.splitext(os.path.basename(filename))[0]
+    name = '%s / %s' % (category, basename)
+    return '* [%s](%s)' % (name, link)
+
+def gen_index(folder, template, target, placeholder, ext='.md'):
+    articles = get_articles(folder, ext)
+    links = map(gen_link, articles)
+
     index = '\n'.join(links)
     with open(template, 'r') as f1, open(target, 'w') as f2:
         content = f1.read()
@@ -30,8 +32,9 @@ def gen_index(folder, template, target, placeholder):
         f2.write(content)
 
 if __name__ == '__main__':
-    folder = os.getenv('folder', '.')
+    folder = os.getenv('folder', 'content')
     template = os.getenv('template', 'README.md.tmpl')
     target = os.getenv('target', 'README.md')
     placeholder = os.getenv('placeholder', '{{__index__}}')
-    gen_index(folder, template, target, placeholder)
+
+    gen_index(folder, template, target, placeholder, ext='.md')
